d8f6f4c2d6b2c018b5c0777d992798a459549e53
Zhang Honghao <zhh358@gmail.com>
2014-02-05 01:36:56 -0500

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by Zhang Honghao on 2/5/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ StoreSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoreSearch
+//
+//  Created by Zhang Honghao on 2/5/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
