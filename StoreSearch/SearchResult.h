c54489ae6a462731d1ca9162b5ed6ff2a76b0034
Zhang Honghao <zhh358@gmail.com>
2014-02-05 14:22:01 -0500

    added searchBar and TableView

---
+++ StoreSearch/SearchResult.h
@@ -0,0 +1,16 @@
+//
+//  SearchResult.h
+//  StoreSearch
+//
+//  Created by Zhang Honghao on 2/5/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchResult : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *artistName;
+
+@end
---
+++ StoreSearch/SearchResult.m
@@ -0,0 +1,13 @@
+//
+//  SearchResult.m
+//  StoreSearch
+//
+//  Created by Zhang Honghao on 2/5/14.
+//  Copyright (c) 2014 org-honghao. All rights reserved.
+//
+
+#import "SearchResult.h"
+
+@implementation SearchResult
+
+@end
